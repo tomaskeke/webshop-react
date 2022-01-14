270a37fcddf028f7b8cae0a09c363948f33c1ea7
Oskar Fjellen <olleboll@hotmail.com>
2022-01-14 11:07:15 +0100

    navbar login

--- src/components/general/NavBar/NavBar.jsx
+++ src/components/general/NavBar/NavBar.jsx
@@ -1,78 +1,82 @@
-import * as React from 'react';
-import { styled, useTheme } from '@mui/material/styles';
-import {Paper, Switch} from '@mui/material/';
-import Button from '@mui/material/Button';
-import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
-import CssBaseline from '@mui/material/CssBaseline';
-import MuiAppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
-import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import ListItem from '@mui/material/ListItem';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import logo from '../pictures/devshop-logo-white.png';
-import CableIcon from '@mui/icons-material/Cable';
-import FemaleIcon from '@mui/icons-material/Female';
-import MaleIcon from '@mui/icons-material/Male';
-import CheckroomIcon from '@mui/icons-material/Checkroom';
+import * as React from "react";
+import { styled, useTheme } from "@mui/material/styles";
+import { Paper, Switch } from "@mui/material/";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import CssBaseline from "@mui/material/CssBaseline";
+import MuiAppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import List from "@mui/material/List";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import ListItem from "@mui/material/ListItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import logo from "../pictures/devshop-logo-white.png";
+import CableIcon from "@mui/icons-material/Cable";
+import FemaleIcon from "@mui/icons-material/Female";
+import MaleIcon from "@mui/icons-material/Male";
+import CheckroomIcon from "@mui/icons-material/Checkroom";
+import { useContext } from "react";
+import { UserContext } from "../../../Context/UserContext";
 
 const drawerWidth = 240;
 
-const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
+
+const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })(
   ({ theme, open }) => ({
     flexGrow: 1,
     padding: theme.spacing(3),
-    transition: theme.transitions.create('margin', {
+    transition: theme.transitions.create("margin", {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
     }),
     marginLeft: `-${drawerWidth}px`,
     ...(open && {
-      transition: theme.transitions.create('margin', {
+      transition: theme.transitions.create("margin", {
         easing: theme.transitions.easing.easeOut,
         duration: theme.transitions.duration.enteringScreen,
       }),
       marginLeft: 0,
     }),
-  }),
+  })
 );
 
 const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== 'open',
+  shouldForwardProp: (prop) => prop !== "open",
 })(({ theme, open }) => ({
-  transition: theme.transitions.create(['margin', 'width'], {
+  transition: theme.transitions.create(["margin", "width"], {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.leavingScreen,
   }),
   ...(open && {
     width: `calc(100% - ${drawerWidth}px)`,
     marginLeft: `${drawerWidth}px`,
-    transition: theme.transitions.create(['margin', 'width'], {
+    transition: theme.transitions.create(["margin", "width"], {
       easing: theme.transitions.easing.easeOut,
       duration: theme.transitions.duration.enteringScreen,
     }),
   }),
 }));
 
-const DrawerHeader = styled('div')(({ theme }) => ({
-  display: 'flex',
-  alignItems: 'center',
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
   padding: theme.spacing(0, 1),
   // necessary for content to be below app bar
   ...theme.mixins.toolbar,
-  justifyContent: 'flex-end',
+  justifyContent: "flex-end",
 }));
 
-export default function PersistentDrawerLeft({dark, setDark}) {
+export default function PersistentDrawerLeft({ dark, setDark }) {
   const theme = useTheme();
   const [open, setOpen] = React.useState(false);
+  const { loggedIn, setLoggedIn, setUser } = useContext(UserContext);
 
   const handleDrawerOpen = () => {
     setOpen(true);
@@ -83,99 +87,216 @@ export default function PersistentDrawerLeft({dark, setDark}) {
   };
 
   const handleDarkMode = () => {
-    if(dark){
-      setDark(false)
-    }else{
-      setDark(true)
+    if (dark) {
+      setDark(false);
+    } else {
+      setDark(true);
     }
+  };
+
+  const handleLogOut = () => {
+    setLoggedIn(false);
+    setUser({ username: "" });
+    // navigate("/login");
   }
 
-  return (
-    <Box sx={{ flexGrow: 1 }}>
-      <CssBaseline />
-      <AppBar position="static" open={open} >
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            edge="start"
-            sx={{ mr: 2, ...(open && { display: 'none' }) }}
-          >
-          <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" noWrap component="div" sx={{ mt: 2, flexGrow: 1}}>
-          <img src={logo}/>
-          </Typography>
-         
-          <Box>
-          <Button color="inherit">Login</Button>
-          </Box>
-    
-        </Toolbar>
-      </AppBar>
-      <Drawer
-        sx={{
-          width: drawerWidth,
-          flexShrink: 0,
-          '& .MuiDrawer-paper': {
+  const authNav = () => {
+    return (
+      <Box sx={{ flexGrow: 1 }}>
+        <CssBaseline />
+        <AppBar position="static" open={open}>
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              onClick={handleDrawerOpen}
+              edge="start"
+              sx={{ mr: 2, ...(open && { display: "none" }) }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ mt: 2, flexGrow: 1 }}
+            >
+              <img src={logo} />
+            </Typography>
+
+            <Box>
+              <Button onClick={handleLogOut} color="inherit">Logout</Button>
+            </Box>
+          </Toolbar>
+        </AppBar>
+
+        <Drawer
+          sx={{
             width: drawerWidth,
-            boxSizing: 'border-box',
-          },
-        }}
-        variant="persistent"
-        anchor="left"
-        open={open}
-      >
-        <DrawerHeader>
-        <Box sx={{textAlign: 'center'}}>
-          <Typography sx={{fontSize: '10px'}}>Toggle Darkmode</Typography>
-        <Switch onClick={handleDarkMode} color="primary"/>          
-          </Box>
-        <IconButton onClick={handleDrawerClose}>
-            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
-          </IconButton>
-        </DrawerHeader>
-        <Divider />
-        <List>
+            flexShrink: 0,
+            "& .MuiDrawer-paper": {
+              width: drawerWidth,
+              boxSizing: "border-box",
+            },
+          }}
+          variant="persistent"
+          anchor="left"
+          open={open}
+        >
+          <DrawerHeader>
+            <Box sx={{ textAlign: "center" }}>
+              <Typography sx={{ fontSize: "10px" }}>Toggle Darkmode</Typography>
+              <Switch onClick={handleDarkMode} color="primary" />
+            </Box>
+            <IconButton onClick={handleDrawerClose}>
+              {theme.direction === "ltr" ? (
+                <ChevronLeftIcon />
+              ) : (
+                <ChevronRightIcon />
+              )}
+            </IconButton>
+          </DrawerHeader>
+          <Divider />
+          <List>
             <ListItem button>
               <ListItemIcon>
-              <MaleIcon />
+                <MaleIcon />
               </ListItemIcon>
               <ListItemText primary={"Men's Clothing"} />
             </ListItem>
             <ListItem button>
               <ListItemIcon>
-              <FemaleIcon />
+                <FemaleIcon />
               </ListItemIcon>
               <ListItemText primary={"Women's Clothing"} />
             </ListItem>
             <ListItem button>
               <ListItemIcon>
-              <CheckroomIcon />
+                <CheckroomIcon />
               </ListItemIcon>
               <ListItemText primary={"Accessories"} />
             </ListItem>
             <ListItem button>
               <ListItemIcon>
-              <CableIcon />
+                <CableIcon />
               </ListItemIcon>
               <ListItemText primary={"Electronics"} />
             </ListItem>
-        </List>
-        <Divider />
-        <List>
+          </List>
+          <Divider />
+          <List>
             <ListItem button>
-              <ListItemIcon>
-              
-              </ListItemIcon>
+              <ListItemIcon></ListItemIcon>
               <ListItemText />
             </ListItem>
-        </List>
-      </Drawer>
-      <Main open={open}>
-        <DrawerHeader />
-      </Main>
-    </Box>
-  );
-}
+          </List>
+        </Drawer>
+        <Main open={open}>
+          <DrawerHeader />
+        </Main>
+      </Box>
+    );
+  };
+
+  const unAuthNav = () => {
+    return (
+      <Box sx={{ flexGrow: 1 }}>
+        <CssBaseline />
+        <AppBar position="static" open={open}>
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              onClick={handleDrawerOpen}
+              edge="start"
+              sx={{ mr: 2, ...(open && { display: "none" }) }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ mt: 2, flexGrow: 1 }}
+            >
+              <img src={logo} />
+            </Typography>
+
+            <Box>
+              <Button onClick={() => setLoggedIn(true)} color="inherit">Login</Button>
+               {/* loggin here */}
+              <Button color="inherit">Register</Button>
+            </Box>
+          </Toolbar>
+        </AppBar>
+
+        <Drawer
+          sx={{
+            width: drawerWidth,
+            flexShrink: 0,
+            "& .MuiDrawer-paper": {
+              width: drawerWidth,
+              boxSizing: "border-box",
+            },
+          }}
+          variant="persistent"
+          anchor="left"
+          open={open}
+        >
+          <DrawerHeader>
+            <Box sx={{ textAlign: "center" }}>
+              <Typography sx={{ fontSize: "10px" }}>Toggle Darkmode</Typography>
+              <Switch onClick={handleDarkMode} color="primary" />
+            </Box>
+            <IconButton onClick={handleDrawerClose}>
+              {theme.direction === "ltr" ? (
+                <ChevronLeftIcon />
+              ) : (
+                <ChevronRightIcon />
+              )}
+            </IconButton>
+          </DrawerHeader>
+          <Divider />
+          <List>
+            <ListItem button>
+              <ListItemIcon>
+                <MaleIcon />
+              </ListItemIcon>
+              <ListItemText primary={"Men's Clothing"} />
+            </ListItem>
+            <ListItem button>
+              <ListItemIcon>
+                <FemaleIcon />
+              </ListItemIcon>
+              <ListItemText primary={"Women's Clothing"} />
+            </ListItem>
+            <ListItem button>
+              <ListItemIcon>
+                <CheckroomIcon />
+              </ListItemIcon>
+              <ListItemText primary={"Accessories"} />
+            </ListItem>
+            <ListItem button>
+              <ListItemIcon>
+                <CableIcon />
+              </ListItemIcon>
+              <ListItemText primary={"Electronics"} />
+            </ListItem>
+          </List>
+          <Divider />
+          <List>
+            <ListItem button>
+              <ListItemIcon></ListItemIcon>
+              <ListItemText />
+            </ListItem>
+          </List>
+        </Drawer>
+        <Main open={open}>
+          <DrawerHeader />
+        </Main>
+      </Box>
+    );
+  };
+
+  return <>{loggedIn ? authNav() : unAuthNav()}</>;
+};
