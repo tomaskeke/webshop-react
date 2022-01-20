dd1e2bf7f046cffdf3cf6b6c42bfe3be9f83f266
Oskar Fjellen <olleboll@hotmail.com>
2022-01-20 21:16:40 +0100

    checkout utloggad

--- src/App.js
+++ src/App.js
@@ -38,9 +38,9 @@ function App() {
             <Route 
             path="checkout"
             element={
-              <PrivateRoute>
+              <UnPrivateRoute>
                 <Checkout />
-              </PrivateRoute>
+              </UnPrivateRoute>
             }
             ></Route>
             <Route
